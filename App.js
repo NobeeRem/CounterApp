5d334b1e42dc1e3a0c952a9b08564daa05b98dca
NobeeRem <144291763+NobeeRem@users.noreply.github.com>
2023-10-06 09:11:06 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+ import logo from "./logo.svg";import "./App.css";import Counter from "./Counter";function App() {
+ return (
+   <div className="App">
+     <h1>Counter App</h1>
+     <Counter />
+   </div>
+ );}
+export default App;
